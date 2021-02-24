374008fe13f61dd5d7a718a6f46f571a2d7eb313
emrecirakoglu <emrecirakoglux@gmail.com>
2021-02-25 01:01:54 +0300

    completed form validation

--- main.js
+++ main.js
@@ -1,41 +1,64 @@
 const submitBtn = document.querySelector(".submit-btn");
 const emailInput = document.getElementById("email");
 const errorMessage = document.querySelector(".error-message");
+const emailTemplate = document.querySelector(".label");
 
 submitBtn.onclick = () => {
-  if (!isValid()) {
-    emailInput.classList.add("input-error");
-    errorMessage.classList.add("show");
+  if (isEmpty() || !isEmailValid()) {
+    showErrorMessage();
+  } else {
+    alert("Registered..");
   }
 };
 
-emailInput.onkeyup = (event) => {
-  if (!isValid()) {
-    emailInput.classList.add("input-error");
-    errorMessage.style.display="block";
-  } else {
-    emailInput.classList.remove("input-error");
-    errorMessage.style.display="none";
+emailInput.onfocus = () => {
+  if (emailTemplate.style.display === "block") {
+    hideEmailTemplate();
   }
 };
 
 emailInput.addEventListener("focusout", () => {
-  console.log("focusout");
-  if (!isValid()) {
-    emailInput.classList.add("input-error");
-    errorMessage.style.display="block";
-  } else {
-    emailInput.classList.remove("input-error");
-    errorMessage.style.display="none";
+  if (isEmpty()) {
+    showErrorMessage();
+    showEmailTemplate();
+  }
+  if (!isEmailValid()) {
+    showErrorMessage();
   }
 });
 
-const eventHandler = () => {}
-
-const isValid = () => {
-  let emailValue = emailInput.value;
-  if (emailValue) {
-    return true;
+emailInput.onkeyup = () => {
+  if (!isEmailValid() || isEmpty()) {
+    showErrorMessage();
+  } else {
+      removeErrorMessage();
+      hideEmailTemplate();
   }
 };
 
+isEmpty = () => {
+  return emailInput.value ? false : true;
+};
+
+isEmailValid = () => {
+  let emailValue = emailInput.value;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(emailValue).toLowerCase());
+};
+
+showErrorMessage = () => {
+  emailInput.classList.add("input-error");
+  errorMessage.style.display = "block";
+};
+removeErrorMessage = () => {
+  emailInput.classList.remove("input-error");
+  errorMessage.style.display = "none";
+};
+
+showEmailTemplate = () => {
+  emailTemplate.style.display = "block";
+};
+
+hideEmailTemplate = () => {
+  emailTemplate.style.display = "none";
+};
